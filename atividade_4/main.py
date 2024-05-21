5503e4b2b8abf52f3ffa3dd1e53dbeab88ce0b50
MarcoAurélioO. <97692272+print-MarcoAurelio@users.noreply.github.com>
2024-05-21 19:24:29 -0300

    atividade 4
    novas funções

--- atividade_4/main.py
+++ atividade_4/main.py
@@ -19,3 +19,21 @@ consumos_km_1 = [
 ]
 
 distancia = float(input("Distancia a ser percorrida (KM): "))
+preco_gasolina = float(input("Preço da gasolina (R$/L): "))
+
+resultados = []
+for modelo_carro, consumos_km_1 in zip(modelos_carros, consumos_km_1):
+    resultado = calcular_consumo_combustivel(
+        modelo_carro, consumos_km_1, distancia, preco_gasolina)
+    resultados.append({
+        'modelo_carro': modelo_carro,
+        **resultado,
+    })
+
+for resultado in resultados:
+    print(f"\n**{resultado['modelo_carro']}**")
+    print(f"Consumo de combustivel: {resultado['consumo_litros']:.2f}")
+    print(f"Custo total: R${resultado['custo_total']:.2f}")
+
+carro_mais_economico = min(resultados, key=lambda x: x['consumo_litros'])
+print(f"\n**Carro mais econômico:** {carro_mais_economico['modelo_carro']}")
